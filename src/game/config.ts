4ae52154d1c199db8791d39dc62e615eb4251553
HGRKzkzk <31103706+HGRKzkzk@users.noreply.github.com>
2026-09-21 22:29:53 +0200

    Update src/game/config.ts

--- src/game/config.ts
+++ src/game/config.ts
@@ -1,8 +1,10 @@
 export const gameConfig = {
-  roundDurationSeconds: 17,
   blocksPerPath: 7,
   pathsPerDirectionPhase: 3,
   pointsPerBlock: 1,
   pointsPerCompletedPath: 3,
   penaltyPerMistake: 1,
+  progressForStageWin: 25,
+  mistakesFromStartToGameOver: 6,
+  barMovementPixels: 12,
 } as const
